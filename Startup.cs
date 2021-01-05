b4d73caed96850ca6d148aacd3e6b9f608f0a840
bsfbruno <brunosuportefs@gmail.com>
2021-01-04 21:40:40 -0300

    Criando areas

--- Startup.cs
+++ Startup.cs
@@ -31,6 +31,10 @@ namespace Dev.UI.Site
             app.UseMvc(routes =>
             {
                 routes.MapRoute(
+                   name: "areas",
+                   template: "{area:exists}/{controller=Home}/{action=Index}/{id?}");
+
+                routes.MapRoute(
                     name: "default",
                     template: "{controller=Home}/{action=Index}/{id?}");
             });
